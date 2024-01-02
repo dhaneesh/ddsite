f380a41fc6b932c8c29abfd23e0712238f0355c5
EC2 Default User <ec2-user@ip-172-31-21-96.eu-central-1.compute.internal>
2024-01-02 11:22:29 +0000

    updated code.

--- drupal/sites/default/settings.php
+++ drupal/sites/default/settings.php
@@ -427,8 +427,11 @@ $settings['reverse_proxy_addresses'] = [$_SERVER['REMOTE_ADDR']];
  * @see \Symfony\Component\HttpFoundation\Request::HEADER_FORWARDED
  * @see \Symfony\Component\HttpFoundation\Request::setTrustedProxies
  */
-$settings['reverse_proxy_trusted_headers'] = \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_ALL;
-
+$settings['reverse_proxy_trusted_headers'] = \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_FOR |
+  \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_HOST |
+  \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_PORT |
+  \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_PROTO |
+  \Symfony\Component\HttpFoundation\Request::HEADER_FORWARDED;
 
 /**
  * Page caching:
